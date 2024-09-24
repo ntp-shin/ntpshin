1c6566a3fc7e95afcd9a8eedcddbe8c5a980b364
Tấn Phát <76056976+ntp-shin@users.noreply.github.com>
2024-09-25 01:52:05 +0700

    4. Edit next-sitemap.config.js (again 2)
    Quay lai ban dau- Here we go again

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,24 +1,24 @@
 const { CONFIG } = require("./site.config")
 
-// module.exports = {
-//   siteUrl: CONFIG.link,
-//   generateRobotsTxt: true,
-//   sitemapSize: 7000,
-//   generateIndexSitemap: false,
-// }
-
 module.exports = {
   siteUrl: CONFIG.link,
-  generateRobotsTxt: true, // Tiếp tục tạo robots.txt
+  generateRobotsTxt: true,
   sitemapSize: 7000,
   generateIndexSitemap: false,
-  robotsTxtOptions: {
-    additionalSitemaps: [], // Xóa các dòng Host liên quan
-    policies: [
-      {
-        userAgent: "*",
-        allow: "/",  // Cho phép toàn bộ trang web được crawl
-      },
-    ],
-  },
 }
+
+// module.exports = {
+//   siteUrl: CONFIG.link,
+//   generateRobotsTxt: true, // Tiếp tục tạo robots.txt
+//   sitemapSize: 7000,
+//   generateIndexSitemap: false,
+//   robotsTxtOptions: {
+//     additionalSitemaps: [], // Xóa các dòng Host liên quan
+//     policies: [
+//       {
+//         userAgent: "*",
+//         allow: "/",  // Cho phép toàn bộ trang web được crawl
+//       },
+//     ],
+//   },
+// }
